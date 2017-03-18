039902225314095f02bad265ce0e0e31d5ff46a3
Darren Wu <darren.wu@canonical.com>
2017-03-18 13:51:35 +0800

    move redpine driver reload function to individual function call

--- hibernate-handler.c
+++ hibernate-handler.c
@@ -11,48 +11,45 @@
 #include <string.h>
 #include <errno.h>
 
-#define init_module(mod, len, opts) syscall(__NR_init_module, mod, len, opts)
 #define finit_module(fd, uargs, flags) syscall(__NR_finit_module, fd, uargs, flags)
 #define delete_module(name, flags) syscall(__NR_delete_module, name, flags)
 
 #define CARACALLA_HANDLER_ENABLE_FILE "/snap/caracalla/current/caracalla-hibernate-handler-enable"
 
-int main(void) {
-    struct utsname utsname={0};
-    char rsi_sdio_ko[128]={0};
-    char rsi_91x_ko[128]={0};
-    int fd_sdio, fd_91x;
+#define BUFFER_SZ 256
+
+int Reload_redpine_driver(void) {
     int ret=EXIT_SUCCESS;
-    struct stat st;
-
-    setlogmask (LOG_UPTO (LOG_NOTICE));
-    openlog ("hibernate-handler", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-
-    if( access( CARACALLA_HANDLER_ENABLE_FILE , F_OK ) == -1 ) {
-        syslog (LOG_INFO, "hibernate-handler is disabled\n");
-        return ret;
-    }
+    char rsi_sdio_ko[BUFFER_SZ]={0};
+    char rsi_91x_ko[BUFFER_SZ]={0};
+    char buf[BUFFER_SZ]={0};
+    struct utsname utsname={0};
+    int fd_sdio, fd_91x;
 
 	syslog (LOG_NOTICE, "Redpine-hibernate reload driver start.\n");
 
 	//Remove driver
     if (delete_module("ven_rsi_sdio", O_NONBLOCK) != 0) {
-        syslog (LOG_ERR, "Remove driver ven_rsi_sdio module failed\n");
-        perror("delete_module failed (ven_rsi_sdio)");
+        syslog (LOG_ERR, "Remove driver vmodule en_rsi_sdio module failed\n");
+        ret = EXIT_FAILURE;
+        goto out;
     }
 
     if (delete_module("ven_rsi_91x", O_NONBLOCK) != 0) {
-        syslog (LOG_ERR, "Remove driver ven_rsi_91x module failed\n");
-        perror("delete_module failed (ven_rsi_91x)");
+        syslog (LOG_ERR, "Remove driver module ven_rsi_91x failed\n");
+        ret = EXIT_FAILURE;
+        goto out;
     }
 
     uname(&utsname);
-    snprintf(rsi_sdio_ko, strlen(utsname.release), "/lib/modules/%s/kernel/ubuntu/rsi/ven_rsi_sdio.ko", utsname.release);
-    snprintf(rsi_91x_ko, strlen(utsname.release), "/lib/modules/%s/kernel/ubuntu/rsi/ven_rsi_91x.ko", utsname.release);
+
+    snprintf(rsi_sdio_ko, BUFFER_SZ, "/lib/modules/%s/kernel/ubuntu/rsi/ven_rsi_sdio.ko", utsname.release);
+    snprintf(rsi_91x_ko, BUFFER_SZ, "/lib/modules/%s/kernel/ubuntu/rsi/ven_rsi_91x.ko", utsname.release);
     fd_sdio = open(rsi_sdio_ko, O_RDONLY);
     if (fd_sdio < 0)
     {
-        syslog (LOG_ERR, "Open ven_rsi_sdio failed, errno:%d\n", errno);
+        syslog (LOG_ERR, "Open driver module %s failed, errno:%d\n", rsi_sdio_ko, errno);
+        perror("Open driver module ven_rsi_sdio.ko failed");
         ret = EXIT_FAILURE;
         goto out;
     }
@@ -60,21 +57,24 @@ int main(void) {
     fd_91x = open(rsi_91x_ko, O_RDONLY);
     if (fd_91x < 0)
     {
-        syslog (LOG_ERR, "Open ven_rsi_91x failed, errno:%d\n", errno);
+        syslog (LOG_ERR, "Open driver module %s failed, errno:%d\n", rsi_91x_ko, errno);
+        perror("Open driver module ven_rsi_91x.ko failed");
         ret = EXIT_FAILURE;
         goto out_close_sdio;
     }
 
     // init_module ven_rsi_91x
     if (finit_module(fd_91x, "", 0) != 0) {
-        syslog (LOG_ERR, "Load module ven_rsi_91x failed, errno:%d\n", errno);
+        syslog (LOG_ERR, "Load driver module ven_rsi_91x failed, errno:%d\n", errno);
+        perror("Load module ven_rsi_91x failed");
         return EXIT_FAILURE;
         goto out_err_close_91x;
     }
 
     // init_module ven_rsi_sdio
     if (finit_module(fd_sdio, "", 0) != 0) {
-        syslog (LOG_ERR, "Load module ven_rsi_sdio failed, errno:%d\n", errno);
+        syslog (LOG_ERR, "Load driver module ven_rsi_sdio failed, errno:%d\n", errno);
+        perror("Load module ven_rsi_sdio failed");
         return EXIT_FAILURE;
         goto out_err_close_91x;
     }
@@ -84,6 +84,22 @@ out_err_close_91x:
 out_close_sdio:
     close(fd_sdio);
 out:
+    return ret;
+}
+
+int main(void) {
+    int ret=EXIT_SUCCESS;
+    struct stat st;
+
+    setlogmask (LOG_UPTO (LOG_NOTICE));
+    openlog ("hibernate-handler", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+    if( access( CARACALLA_HANDLER_ENABLE_FILE , F_OK ) == -1 ) {
+        syslog (LOG_ERR, "hibernate-handler is disabled\n");
+        return ret;
+    }
+    
+    ret = Reload_redpine_driver();
     closelog();
     return ret;
 }
