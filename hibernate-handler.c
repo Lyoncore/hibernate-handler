7a0d9d8e8bad3a0072155c6e2dc8fb4b1edf5f54
Darren Wu <darren.wu@canonical.com>
2017-03-21 18:20:31 +0800

    fixed phy number changed after resume

--- hibernate-handler.c
+++ hibernate-handler.c
@@ -22,9 +22,11 @@ int Reload_redpine_driver(void) {
     int ret=EXIT_SUCCESS;
     char rsi_sdio_ko[BUFFER_SZ]={0};
     char rsi_91x_ko[BUFFER_SZ]={0};
+    char mac80211_ko[BUFFER_SZ]={0};
+    char cfg80211_ko[BUFFER_SZ]={0};
     char buf[BUFFER_SZ]={0};
     struct utsname utsname={0};
-    int fd_sdio, fd_91x;
+    int fd_sdio, fd_91x, fd_mac80211, fd_cfg80211;
 
     syslog (LOG_NOTICE, "Redpine-hibernate reload driver start.\n");
 
@@ -41,10 +43,24 @@ int Reload_redpine_driver(void) {
         goto out;
     }
 
+    if (delete_module("mac80211", O_NONBLOCK) != 0) {
+        syslog (LOG_ERR, "Remove driver module mac80211 failed\n");
+        ret = EXIT_FAILURE;
+        goto out;
+    }
+
+    if (delete_module("cfg80211", O_NONBLOCK) != 0) {
+        syslog (LOG_ERR, "Remove driver module cfg80211 failed\n");
+        ret = EXIT_FAILURE;
+        goto out;
+    }
+
     uname(&utsname);
 
     snprintf(rsi_sdio_ko, BUFFER_SZ, "/lib/modules/%s/kernel/ubuntu/rsi/ven_rsi_sdio.ko", utsname.release);
     snprintf(rsi_91x_ko, BUFFER_SZ, "/lib/modules/%s/kernel/ubuntu/rsi/ven_rsi_91x.ko", utsname.release);
+    snprintf(mac80211_ko, BUFFER_SZ, "/lib/modules/%s/kernel/net/mac80211/mac80211.ko", utsname.release);
+    snprintf(cfg80211_ko, BUFFER_SZ, "/lib/modules/%s/kernel/net/wireless/cfg80211.ko", utsname.release);
     fd_sdio = open(rsi_sdio_ko, O_RDONLY);
     if (fd_sdio < 0)
     {
@@ -63,12 +79,46 @@ int Reload_redpine_driver(void) {
         goto out_close_sdio;
     }
 
+    fd_mac80211 = open(mac80211_ko, O_RDONLY);
+    if (fd_mac80211 < 0)
+    {
+        syslog (LOG_ERR, "Open driver module %s failed, errno:%d\n", mac80211_ko, errno);
+        perror("Open driver module mac80211.ko failed");
+        ret = EXIT_FAILURE;
+        goto out_close_91x;
+    }
+
+    fd_cfg80211 = open(cfg80211_ko, O_RDONLY);
+    if (fd_cfg80211 < 0)
+    {
+        syslog (LOG_ERR, "Open driver module %s failed, errno:%d\n", cfg80211_ko, errno);
+        perror("Open driver module cfg80211.ko failed");
+        ret = EXIT_FAILURE;
+        goto out_close_mac80211;
+    }
+
+    // init_module cfg80211
+    if (finit_module(fd_cfg80211, "", 0) != 0) {
+        syslog (LOG_ERR, "Load driver module cfg80211 failed, errno:%d\n", errno);
+        perror("Load module cfg80211 failed");
+        return EXIT_FAILURE;
+        goto out_close_cfg80211;
+    }
+
+    // init_module mac80211
+    if (finit_module(fd_mac80211, "", 0) != 0) {
+        syslog (LOG_ERR, "Load driver module mac80211 failed, errno:%d\n", errno);
+        perror("Load module mac80211 failed");
+        return EXIT_FAILURE;
+        goto out_close_cfg80211;
+    }
+
     // init_module ven_rsi_91x
     if (finit_module(fd_91x, "", 0) != 0) {
         syslog (LOG_ERR, "Load driver module ven_rsi_91x failed, errno:%d\n", errno);
         perror("Load module ven_rsi_91x failed");
         return EXIT_FAILURE;
-        goto out_err_close_91x;
+        goto out_close_cfg80211;
     }
 
     // init_module ven_rsi_sdio
@@ -76,15 +126,21 @@ int Reload_redpine_driver(void) {
         syslog (LOG_ERR, "Load driver module ven_rsi_sdio failed, errno:%d\n", errno);
         perror("Load module ven_rsi_sdio failed");
         return EXIT_FAILURE;
-        goto out_err_close_91x;
+        goto out_close_cfg80211;
     }
+
 
     syslog (LOG_NOTICE, "Redpine-hibernate reload driver complete.\n");
 
-out_err_close_91x:
+out_close_cfg80211:
+    close(fd_cfg80211);
+out_close_mac80211:
+    close(fd_mac80211);
+out_close_91x:
     close(fd_91x);
 out_close_sdio:
     close(fd_sdio);
+
 out:
     return ret;
 }
